48e622785d3a15f4793008297dc2de1bedd6923b
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2018-07-23 10:35:50 +0300

    add codeSecurityService template

--- src/main/java/codesecurity/api/service/CodeSecurityService.java
+++ src/main/java/codesecurity/api/service/CodeSecurityService.java
@@ -4,42 +4,30 @@ import com.capitalone.dashboard.model.CollectorItem;
 import com.capitalone.dashboard.model.CollectorType;
 import com.capitalone.dashboard.model.Component;
 import com.capitalone.dashboard.model.DataResponse;
-import com.capitalone.dashboard.repository.CollectorRepository;
-import com.capitalone.dashboard.repository.ComponentRepository;
 import org.bson.types.ObjectId;
 import com.google.common.collect.Iterables;
 
 import java.util.List;
-public abstract class CodeSecurityService<RepoT, T, Req> {
+public abstract class CodeSecurityService<Type, Req>{
 
-    protected final RepoT securityRepository;
-    protected final ComponentRepository componentRepository;
-    protected final CollectorRepository collectorRepository;
+    public abstract DataResponse<Iterable<Type>> search(Req request);
 
-    public CodeSecurityService(RepoT securityRepository,
-                               ComponentRepository componentRepository,
-                               CollectorRepository collectorRepository) {
-        this.securityRepository = securityRepository;
-        this.componentRepository = componentRepository;
-        this.collectorRepository = collectorRepository;
-    }
-
-    public abstract DataResponse<Iterable<T>> search(Req request);
-
-    public abstract DataResponse<Iterable<T>> searchType(Req request);
+    public abstract DataResponse<Iterable<Type>> searchType(Req request);
 
     protected abstract ObjectId getRequestId(Req request);
 
-    protected abstract CollectorType getCollectortype();
+    protected abstract CollectorType getCollectorType();
 
-    protected DataResponse<Iterable<T>> emptyResponse() {
+    protected abstract Component getComponent(ObjectId id);
+
+    protected DataResponse<Iterable<Type>> emptyResponse() {
         return new DataResponse<>(null, System.currentTimeMillis());
     }
 
     protected CollectorItem getCollectorItem(Req request) {
         CollectorItem item = null;
-        Component component = componentRepository.findOne(getRequestId(request));
-        List<CollectorItem> items = component.getCollectorItems().get(getCollectortype());
+        Component component = getComponent(getRequestId(request));
+        List<CollectorItem> items = component.getCollectorItems().get(getCollectorType());
         if (items != null) {
             item = Iterables.getFirst(items, null);
         }
